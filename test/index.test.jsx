062e132b65a333ed6c7b30f281ff8571558983a9
zhid0399123 <zhid0399123@gmail.com>
2024-06-10 00:58:30 +0300

    refactored
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,28 +1,28 @@
-import ReactNotification from "../src"
-import { it, vi, assert, afterEach } from "vitest"
-import notificationWrapper from "./fixtures/index.js"
+import ReactNotification from '../src'
+import { it, vi, assert, afterEach } from 'vitest'
+import notificationWrapper from './fixtures/index.js'
 
 // @vitest-environment jsdom
 
-it("should notify and disappear", async ({ expect }) => {
+it('should notify and disappear', async ({ expect }) => {
   document.body.innerHTML = notificationWrapper
   const notification = new ReactNotification({
-    message: "test has passed",
+    message: 'test has passed',
     timeout: 100,
-    root: "root",
-    animation: "animation",
-    formatter(message) {
-      return message.concat(" :)")
+    root: 'root',
+    animation: 'animation',
+    formatter (message) {
+      return message.concat(' :)')
     }
   })
 
-  const p = document.body.querySelector(".root")
-  const notify = vi.spyOn(notification, "notify")
-  const formatter = vi.spyOn(notification, "formatter")
-  const div = document.body.querySelector(".notification")
-  const populateRoot = vi.spyOn(notification, "populateRoot")
+  const p = document.body.querySelector('.root')
+  const notify = vi.spyOn(notification, 'notify')
+  const formatter = vi.spyOn(notification, 'formatter')
+  const div = document.body.querySelector('.notification')
+  const populateRoot = vi.spyOn(notification, 'populateRoot')
 
-  assert.isFalse(div.classList.contains("animation"), "animation class not added")
+  assert.isFalse(div.classList.contains('animation'), 'animation class not added')
 
   notification.notify()
 
@@ -30,23 +30,27 @@ it("should notify and disappear", async ({ expect }) => {
   expect(formatter).toHaveBeenCalled()
   expect(populateRoot).toHaveBeenCalled()
   expect(populateRoot).toHaveReturned()
-  expect(formatter).toHaveReturnedWith("test has passed :)")
+  expect(formatter).toHaveReturnedWith('test has passed :)')
 
-  assert.isOk(p.innerHTML, "returned string")
-  assert.strictEqual(p.innerHTML, "test has passed :)")
-  assert.isTrue(div.classList.contains("animation"), "animation class added")
+  assert.isOk(p.innerHTML, 'returned string')
+  assert.strictEqual(p.innerHTML, 'test has passed :)')
+  assert.isTrue(div.classList.contains('animation'), 'animation class added')
 
   let callback = false
 
-  await vi.waitUntil(async () => await notification.notify(() => callback = true), { timeout: 500 })
+  await vi.waitUntil(async () => {
+     await notification.notify(() => {
+      callback = true
+    })
+    return true
+  }, { timeout: 500 })
 
-  assert.strictEqual(p.innerHTML, "")
-  assert.isTrue(callback, "callback executed")
-  assert.isNotOk(p.innerHTML, "returned empty string")
-  assert.isFalse(div.classList.contains("animation"), "animation class added")
+  assert.strictEqual(p.innerHTML, '')
+  assert.isTrue(callback, 'callback executed')
+  assert.isNotOk(p.innerHTML, 'returned empty string')
+  assert.isFalse(div.classList.contains('animation'), 'animation class added')
 
   afterEach(() => {
     callback = false
   })
-
 })
